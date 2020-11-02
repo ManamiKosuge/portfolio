7d8e948576a42ebbdaca2437cea02c3e456d40a7
ManamiKosuge <70672151+ManamiKosuge@users.noreply.github.com>
2020-11-02 11:00:30 +0900

    9th_add

--- work/js/work_header.js
+++ work/js/work_header.js
@@ -1,13 +1,52 @@
 //作品別ページ＿共通ヘッダー読み込みJS
-function header(rootDir){
-    $.ajax({
-        url: rootDir + "include/work_header.html", // ディレクトリー変更
-        cache: false,
-        async: false,
-        dataType: 'html',
-        success: function(html){
-            html = html.replace(/\{\$root\}/g, rootDir);
-            document.write(html);
-        }
-    });
+function include(filename, afterfunc) {
+
+  include.seq = (include.seq)? include.seq + 1: 1;
+
+  var id = new Date().getTime() + "-" + include.seq;
+  var inc = document.createElement("iframe");
+
+  inc.id = "inc-" + id;
+  inc.src = filename;
+  inc.style.display = "none";
+  document.write("<span id=\"" + id + "\"></span>");
+
+  var incfunc = function() {
+
+    var s = (function() {
+      var suffix = (n = filename.lastIndexOf(".")) >= 0 ? filename.substring(n): "default";
+      if (suffix == ".html") return inc.contentWindow.document.body.innerHTML;
+      if (suffix == ".txt") return inc.contentWindow.document.body.firstChild.innerHTML;
+      if (suffix == "default") return inc.contentWindow.document.body.innerHTML;
+    })();
+
+    var span = document.getElementById(id);
+
+    var insertBeforeHTML = function(htmlStr, refNode) {
+      if (document.createRange) {
+        var range = document.createRange();
+        range.setStartBefore(refNode);
+        refNode.parentNode.insertBefore(range.createContextualFragment(htmlStr), refNode);
+      } else {
+        refNode.insertAdjacentHTML('BeforeBegin', htmlStr);
+      }
+    };
+
+    insertBeforeHTML(s.split("&gt;").join(">").split("&lt;").join("<"), span);
+    document.body.removeChild(inc);
+    span.parentNode.removeChild(span);
+    if (afterfunc) afterfunc();
+  };
+
+  if (window.attachEvent) {
+    window.attachEvent('onload',
+      function() {
+        document.body.appendChild(inc);
+        inc.onreadystatechange = function() { if (this.readyState == "complete") incfunc(); };
+      });
+  }
+  else {
+    document.body.appendChild(inc);
+    inc.onload = incfunc;
+  }
 }
